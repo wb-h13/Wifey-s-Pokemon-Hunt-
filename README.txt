16cc0e607ad45bd02de84b508f365e2ff87bf0be
wb-h13 <will@shoptimised.com>
2026-08-30 11:15:56 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,9 @@
+WIFEY'S POKEMON HUNT - GITHUB PAGES
+
+Upload these files to the root of a GitHub repository:
+- index.html
+- manifest.webmanifest
+- sw.js
+- icons/ folder
+
+Then enable GitHub Pages from Settings > Pages > Deploy from a branch > main / root.
---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='wifeys-pokemon-hunt-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;}).catch(()=>caches.match('./index.html'))));
+});
